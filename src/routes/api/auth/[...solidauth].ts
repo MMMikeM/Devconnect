e26d9168a02e8d14077b09ba48284fd7dbdd2afb
Mike Murray <mikem.tba@gmail.com>
2023-04-23 12:00:02 +0200

    refactor: 🔥 Remove unneeded dependencies and disable debug mode

--- src/routes/api/auth/[...solidauth].ts
+++ src/routes/api/auth/[...solidauth].ts
@@ -1,18 +1,18 @@
 import GitHub from "@auth/core/providers/github"
 import { SolidAuth } from "@solid-auth/base"
 import config from "~/config"
-import KyselyAdapter from "~/utils/auth-adapter"
 import { db } from "~/server/db"
+import KyselyAdapter from "~/utils/auth-adapter"
 
-import type { SolidAuthConfig } from "@solid-auth/base"
 import type { Provider } from "@auth/core/providers"
+import type { SolidAuthConfig } from "@solid-auth/base"
 
 const { clientId, clientSecret } = config.auth.github
 
 export const authOptions: SolidAuthConfig = {
   adapter: KyselyAdapter(db),
   providers: [GitHub({ clientId, clientSecret }) as Provider],
-  debug: true,
+  // debug: true,
 }
 
 export const { GET, POST } = SolidAuth(authOptions)
